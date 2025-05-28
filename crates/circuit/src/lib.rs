8647512d16256a394e8d1e1f32400a64fd798bc5
keyleu <kleuyckx@gmail.com>
2025-05-28 11:38:30 +0100

    authorization pause and unpause test app

--- crates/circuit/src/lib.rs
+++ crates/circuit/src/lib.rs
@@ -4,12 +4,28 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use valence_coprocessor::Witness;
+use valence_authorization_utils::{
+    authorization::AuthorizationMsg, domain::Domain, zk_authorization::ZkMessage,
+};
 
 pub fn circuit(witnesses: Vec<Witness>) -> Vec<u8> {
-    let value = witnesses[0].as_data().unwrap();
-    let value = <[u8; 8]>::try_from(value).unwrap();
-    let value = u64::from_le_bytes(value);
-    let value = value.wrapping_add(1);
+    let registry = witnesses[0].as_data().unwrap();
+    let registry = <[u8; 8]>::try_from(registry).unwrap();
+    let registry = u64::from_le_bytes(registry);
 
-    value.to_le_bytes().to_vec()
+    let message = match registry {
+        1 => AuthorizationMsg::Pause {},
+        2 => AuthorizationMsg::Resume {},
+        _ => panic!("Invalid registry"),
+    };
+
+    let value = ZkMessage {
+        registry,
+        block_number: 1,
+        domain: Domain::Main,
+        authorization_contract: None,
+        message,
+    };
+
+    serde_json::to_vec(&value).unwrap()
 }
